aebd5380974303dbf9a046a8c2a9ad8822d0ce01
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2022-12-13 02:35:48 +0500

    resolve problems with scroll

--- src/index.js
+++ src/index.js
@@ -42,37 +42,12 @@ function init() {
         current = pageNumber
     }
 
-    document.addEventListener('wheel', throttle(scrollChange, 2000));
+    document.addEventListener('wheel', throttle(scrollChange, 2000)); 
     
-    document.addEventListener('touchstart', throttle(scrollChange, 2000));
-    document.addEventListener('touchmove', throttle(scrollChange, 2000));
-
-
-
-    $(document).on('touchstart', function(e) {
-
-        var swipe = e.originalEvent.touches,
-        start = swipe[0].pageY;
     
-        $(this).on('touchmove', function(e) {
-    
-            var contact = e.originalEvent.touches,
-            end = contact[0].pageY,
-            distance = end-start;
-    
-            if (distance < -30) // up
-            if (distance > 30) // down
-        })
-        .one('touchend', function() {
-    
-            $(this).off('touchmove touchend');
-        });
-    });
-    
-
     function scrollChange(e) {
-        console.log(e);
-        if (e.deltaY > 0) {
+        let cl = e.deltaY??e;
+        if (cl > 0) {
             scrollSlide += 1;
         } else {
             scrollSlide -= 1;
@@ -90,17 +65,81 @@ function init() {
         nextSlide(scrollSlide);
     }
 
-    document.querySelector('.poems').addEventListener('click', (e) => {
-            
-            console.log(e.target.classList);
-        });
+    
+
+    /**/
+    document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            /* right swipe */ 
+        } else {
+            /* left swipe */
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            if(!document.querySelector('.content').classList.contains('auto') && !document.querySelectorAll('.page')[document.querySelectorAll('.page').length - 1].classList.contains('auto'))
+                scrollChange(yDiff)
+        } else {
+            if(!document.querySelector('.content').classList.contains('auto') && !document.querySelectorAll('.page')[document.querySelectorAll('.page').length - 1].classList.contains('auto'))
+                scrollChange(yDiff)
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+    /**/
+
+    document.querySelector('.btn-reverse').addEventListener('click', function() {
+        nextSlide(5);
+        document.querySelector('.content').classList.remove('auto');
+        !document.querySelectorAll('.page')[document.querySelectorAll('.page').length - 1].classList.remove('auto')
+    });
     document.querySelector('.btn-start').addEventListener('click', function() {
         nextSlide(6);
+        document.querySelector('.content').classList.add('auto');
+        !document.querySelectorAll('.page')[document.querySelectorAll('.page').length - 1].classList.add('auto');
     });
 
     document.addEventListener('load', nextSlide(0));
 
-
+    document.querySelector('.poems').addEventListener('click', function(e) {
+        let poem = e.target.closest('.poem');
+        if(poem) {
+            let picture = poem.querySelector('.picture');
+            gsap.timeline().to(poem, .6, {width:'100%', height: '100%', position: 'absolute',
+                    left: "50%", top: "50%", translateX: "-50%", translateY: "-50%"}).
+                    to(picture, .6, {display: "flex", width: "50%", justifyContent: "cener", alignItems: "center"});
+        }
+    });
 
 
 
@@ -116,11 +155,13 @@ function throttle(func, limit) {
         const context = this;
         const args = arguments;
         if (!inThrottle) {
-            func.apply(context, args);
-            inThrottle = true;
-            setTimeout(() => {
-                inThrottle = false
-            }, limit);
+            if(!document.querySelector('.content').classList.contains('auto') && !document.querySelectorAll('.page')[document.querySelectorAll('.page').length - 1].classList.contains('auto')){
+                func.apply(context, args);
+                inThrottle = true;
+                setTimeout(() => {
+                    inThrottle = false
+                }, limit);
+            }
         }
     }
 }
